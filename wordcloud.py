236f2452188b68c0ee4b29e0a956825062532304
Narwhalish <bryao@ctemc.org>
2018-05-04 14:20:09 -0400

    Write algorithm to prepare data for visualization
    Algorithm must find the number of states for each energy type that use that for the maximum energy production.

--- wordcloud.py
+++ wordcloud.py
@@ -1,4 +1,24 @@
 import reader
-import pillow
+from PIL import Image
+#initialise data variables
 gens, income = reader.parse_data()
-print gens.
+
+new_gens = gens
+del gens['total']
+new_gens = new_gens.items()
+# 
+# for energy_type in new_gens:
+#     energy_type[1] = max(energy_type[1])
+
+for state in range(52):
+    highest=0
+    tpe=''
+    for power in powers:
+        if power[i]>highest:
+            highest=power[i]
+            tpe=power
+    
+
+
+new_img = Image.new('RGB', (500, 500), color = 0)
+new_canvas = ImageDraw.Draw(new_img)
